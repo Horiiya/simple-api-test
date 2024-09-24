171425663b6983ecf780de22aa5e8a5f8cca2119
Horii <nichapat.uah@gmail.com>
2024-09-24 19:29:22 +0700

    change getcode

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
 
 @app.route("/getcode", methods=["GET"])
 def getcode():
-    return "getcode test123"
+    return "getcode Hello test"
 
 
 @app.route("/plus/<num1>/<num2>", methods=["GET"])
